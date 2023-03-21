523c6f9e6eaa340978792f8d3a164555e6dcd763
amanasj <67763567+amanasj@users.noreply.github.com>
2023-03-21 13:38:58 +0000

    Add files via upload

---
+++ R/BA_normal.R
@@ -0,0 +1,200 @@
+
+#########################################################################################################################
+#############   Function to calculate all Bland-Altman statistics including coefficient of repeatability   ##############          
+#               ----------------------------------------------------------------------------------------                #
+#  Code taken and modified from:                                                                                        #
+#  https://rowannicholls.github.io/R/statistics/agreement/bland_altman_giavarina.html#bland-altman-analysis-1           #
+#  And                                                                                                                  #
+#  https://github.com/deepankardatta/blandr/blob/v.0.5.3-development/R/blandr.statistics.r                              #
+#########################################################################################################################
+
+
+
+
+
+############## test with randomly generated normal dist ####################
+#set.seed(123)
+#x <- rnorm(10, 10, 2)
+#y <- rnorm(10, 10, 2)
+#df <- cbind.data.frame(x,y)
+#x <- df$x
+#y <- df$y
+############################################################################
+
+
+
+
+
+
+############################### BA Stats ###############################
+
+################# Stats function ################
+#################################################
+
+bland_altman_stats <- function(df, x, y, repeated, random, fixed, nboot) {
+  
+  
+  if(repeated==FALSE){ 
+    
+    
+  # Individual sample calculations
+  df$mean <- (x + y) / 2
+  df$diff <- y - x
+  
+  # Whole sample calculations
+  summary <- data.frame()
+  means <- c(paste("mean of x"), paste("mean of y"))
+  summary[1, paste("mean of x")] <- mean(x)
+  summary[1, paste("mean of y")] <- mean(y)
+
+  # Sample size
+  N <- nrow(df)
+  summary[1, "N"] <- N
+  # Degrees of freedom
+  dof <- nrow(df) -1 
+  summary[1, "DoF"] <- dof
+  # Bias (mean difference)
+  bias <- mean(df[["diff"]])
+  summary[1, "bias"] <- bias
+  # Confidence intervals (based on 2nd paper) Based on significance level supplied
+  # (defaults to 95% CI) CI for mean
+  biasSEM <- sd(df[["diff"]])/sqrt(N)
+  # Convert confidence interval to a two-tailed z-value Don't really need this but kept as
+  # a remnant of old version to possibly use in the future
+  alpha <- 0.05
+  sig.level.two.tailed <- 1 - (alpha/2)
+  sig.level.convert.to.z <- qnorm(sig.level.two.tailed)
+  biasCI <- qt(sig.level.two.tailed, df = dof) * biasSEM
+  biasLowerCI <- bias - biasCI
+  biasUpperCI <- bias + biasCI
+  summary[1, "bias_lowerCI"] <- biasLowerCI
+  summary[1, "bias_upperCI"] <- biasUpperCI
+  # Population standard deviation of the differences
+  #  st_dev_diff <- sd(df[["Diff"]]) * sqrt((nrow(df) - 1) / nrow(df))   ### not sure why population sd was used here, not normally done
+  # Sample standard deviation of the differences
+  st_dev_diff <- sd(df[["diff"]])
+  summary[1, "SD_diffs"] <- st_dev_diff
+  lowerLOA <- bias - 1.96 * st_dev_diff
+  upperLOA <- bias + 1.96 * st_dev_diff
+  summary[1, "lower_LoA"] <- lowerLOA
+  summary[1, "upper_LoA"] <- upperLOA
+  # CI for limits of agreement LOAVariance from Carkeet
+  LOAVariance <- ((1/N) + ((sig.level.convert.to.z^2)/(2 * dof))) * st_dev_diff^2
+  LOA_SEM <- sqrt(LOAVariance)
+  LOA_CI <- qt(sig.level.two.tailed, df = dof) * LOA_SEM
+  lowerLOA_lowerCI <- lowerLOA - LOA_CI
+  lowerLOA_upperCI <- lowerLOA + LOA_CI
+  upperLOA_lowerCI <- upperLOA - LOA_CI
+  upperLOA_upperCI <- upperLOA + LOA_CI
+  summary[1, "lowerLOA_lowerCI"] <- lowerLOA_lowerCI
+  summary[1, "lowerLOA_upperCI"] <- lowerLOA_upperCI
+  summary[1, "upperLOA_lowerCI"] <- upperLOA_lowerCI
+  summary[1, "upperLOA_upperCI"] <- upperLOA_upperCI
+  # Within-subject standard deviation
+  xy <- cbind(x,y)
+  df[['sample_SD']] <- apply(xy, 1, sd)
+  df[['sample_variance']] <- df[['sample_SD']]**2
+  s_w <- sqrt(mean(df[['sample_variance']]))
+  summary[1, "within-subject_SD_(Sw)"] <- s_w
+  # Coefficient of repeatability
+  summary[1, "Coefficient_of_Repeatability"] <- sqrt(2) * 1.96 * s_w
+  
+  # Return
+  return(list(df = df, summary = summary))
+  
+  
+results <- bland_altman_stats(df,x,y)
+df <- results[[1]]
+summary <- results[[2]]
+print(summary)
+print(df)
+
+
+
+
+  } else {
+    
+    
+    tryCatch({
+    
+    library(nlme)
+    resa<-lme(diff~as.factor(fixed),random=~1|random,
+              correlation=corCompSymm(form=~1|random),data=df, na.action=na.omit)
+  
+    
+    
+    # Individual sample calculations
+    df$mean <- (x + y) / 2
+    df$diff <- y - x
+    
+    # Whole sample calculations
+    summary <- data.frame()
+    means <- c(paste("mean of x"), paste("mean of y"))
+    summary[1, paste("mean of x")] <- mean(x)
+    summary[1, paste("mean of y")] <- mean(y)
+    
+    # Sample size
+    N <- nrow(df)
+    summary[1, "N"] <- N
+    # Degrees of freedom
+    dof <- nrow(df) -1 
+    summary[1, "DoF"] <- dof
+    # Bias (mean difference)
+    bias <- mean(df[["diff"]])
+    summary[1, "bias"] <- bias
+    # Confidence intervals (based on 2nd paper) Based on significance level supplied
+    # (defaults to 95% CI) CI for mean
+    biasSEM <- sd(df[["diff"]])/sqrt(N)
+    # Convert confidence interval to a two-tailed z-value Don't really need this but kept as
+    # a remnant of old version to possibly use in the future
+    alpha <- 0.05
+    sig.level.two.tailed <- 1 - (alpha/2)
+    sig.level.convert.to.z <- qnorm(sig.level.two.tailed)
+    biasCI <- qt(sig.level.two.tailed, df = dof) * biasSEM
+    biasLowerCI <- bias - biasCI
+    biasUpperCI <- bias + biasCI
+    summary[1, "bias_lowerCI"] <- biasLowerCI
+    summary[1, "bias_upperCI"] <- biasUpperCI
+    # Population standard deviation of the differences
+    #  st_dev_diff <- sd(df[["Diff"]]) * sqrt((nrow(df) - 1) / nrow(df))   ### not sure why population sd was used here, not normally done
+    # Sample standard deviation of the differences
+    st_dev_diff <- sd(df[["diff"]])
+    summary[1, "SD_diffs"] <- st_dev_diff
+    lowerLOA <- bias - 1.96 * st_dev_diff
+    upperLOA <- bias + 1.96 * st_dev_diff
+    summary[1, "lower_LoA"] <- lowerLOA
+    summary[1, "upper_LoA"] <- upperLOA
+    # CI for limits of agreement LOAVariance from Carkeet
+    LOAVariance <- ((1/N) + ((sig.level.convert.to.z^2)/(2 * dof))) * st_dev_diff^2
+    LOA_SEM <- sqrt(LOAVariance)
+    LOA_CI <- qt(sig.level.two.tailed, df = dof) * LOA_SEM
+    lowerLOA_lowerCI <- lowerLOA - LOA_CI
+    lowerLOA_upperCI <- lowerLOA + LOA_CI
+    upperLOA_lowerCI <- upperLOA - LOA_CI
+    upperLOA_upperCI <- upperLOA + LOA_CI
+    summary[1, "lowerLOA_lowerCI"] <- lowerLOA_lowerCI
+    summary[1, "lowerLOA_upperCI"] <- lowerLOA_upperCI
+    summary[1, "upperLOA_lowerCI"] <- upperLOA_lowerCI
+    summary[1, "upperLOA_upperCI"] <- upperLOA_upperCI
+    # Within-subject standard deviation
+    s_w <- as.numeric(VarCorr(resa)[2,2])
+    # Coefficient of repeatability
+    summary[1, "Coefficient_of_Repeatability"] <- sqrt(2) * 1.96 * s_w
+    
+    # Return
+    return(list(df = df, summary = summary))
+    
+    
+    results <- bland_altman_stats(df,x,y)
+    df <- results[[1]]
+    summary <- results[[2]]
+    print(summary)
+    print(df)
+    
+    }, error=function(e){cat("\n \n ERROR : Must SPECIFY: RANDOM' AND 'FIXED'")})
+    
+  }
+
+}
+
+
---
+++ R/BA_plot.R
@@ -0,0 +1,107 @@
+library(tidyverse)
+library(here)
+library(patchwork)
+
+
+
+
+BA_plot <- function(stats,x,y,title=NULL,pointcolour="black"){
+
+
+  
+  
+theme <- theme_bw()+theme(
+  axis.text=element_text(size=16), 
+  axis.title=element_text(size=16,face="bold"),
+  plot.title = element_text(size=18, hjust=0.5,face="bold",
+                            margin = margin(t = 0, b = 0)),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank())
+
+
+
+
+
+###################### extract stats for plots ###########################
+data <- stats$df
+bias <- signif(stats$summary$bias, digits = 3)
+biasLowerCI <- signif(stats$summary$bias_lowerCI, digits = 3)
+biasUpperCI <- signif(stats$summary$bias_upperCI, digits = 3)
+lloa <- signif(stats$summary$lower_LoA, digits = 3)
+uloa <- signif(stats$summary$upper_LoA, digits = 3)
+upperLOA_upperCI <- signif(stats$summary$upperLOA_upperCI, digits = 3)
+upperLOA_lowerCI <- signif(stats$summary$upperLOA_lowerCI, digits = 3)
+lowerLOA_upperCI <- signif(stats$summary$lowerLOA_upperCI, digits = 3)
+lowerLOA_lowerCI <- signif(stats$summary$lowerLOA_lowerCI, digits = 3)
+CoR <- signif(stats$summary$Coefficient_of_Repeatability, digits = 3)
+xmax <- max(x)
+
+
+
+
+
+
+if("stimulus_ID" %in% colnames(data)){
+  baplot <- ggplot(data, aes(mean, diff)) +
+    geom_point(aes(colour=patient_ID))+
+    labs(x = "Average of test 2 & test 3", 
+         y = "Difference (test 2 - test 3)", 
+         title = title,
+         color = "Patient ID")+
+    theme
+  
+} else {
+  baplot <- ggplot(data, aes(mean, diff)) +
+    geom_point(colour=pointcolour)+
+    labs(x = "Average of test 2 & test 3", 
+         y = "Difference (test 2 - test 3)", 
+         title = title)+
+    theme
+}
+  
+  
+  
+
+####change axis limits
+baplot <- baplot + 
+  ggplot2::coord_cartesian(xlim=c(-0.1,xmax+5), ylim=c(lowerLOA_lowerCI-15,upperLOA_upperCI+15), expand = F)
+
+baplot <- baplot+annotate("text", x=xmax, y=biasUpperCI+1, 
+                          label= paste0("bias = ", bias, " (95% CI: ", biasLowerCI, "," , biasUpperCI,")"), 
+                          size=3.5, fontface = 2)
+baplot <- baplot+annotate("text", x=xmax, y=upperLOA_upperCI+3, 
+                          label= paste0("ULoA = ", uloa, " (95% CI: ", upperLOA_lowerCI, "," , upperLOA_upperCI,")"), 
+                          size=5, fontface = 2)
+baplot <- baplot+annotate("text", x=xmax, y=lowerLOA_lowerCI-3, 
+                          label= paste0("LLoA = ", lloa, " (95% CI: ", lowerLOA_lowerCI, "," , lowerLOA_upperCI,")"), 
+                          size=5, fontface = 2) 
+baplot <- baplot+annotate("text", x=xmax, y=upperLOA_upperCI+8, 
+                          label= paste0("CoR = ", CoR), 
+                          size=4, fontface = 2) 
+baplot <- baplot +
+  geom_hline(yintercept = bias , linetype = 2) + # Bias
+  geom_hline(yintercept = biasUpperCI, linetype = 2) + # Bias - upper confidence interval
+  geom_hline(yintercept = biasLowerCI, linetype = 2) + # Bias - lower confidence interval
+  geom_hline(yintercept = uloa, linetype = 2 ) + # ULOA 
+  geom_hline(yintercept = lloa, linetype = 2 ) + # LLOA 
+  geom_hline(yintercept = upperLOA_upperCI, linetype = 3 ) + # Upper limit of agreement - upper confidence interval
+  geom_hline(yintercept = upperLOA_lowerCI, linetype = 3 ) + # Upper limit of agreement - lower confidence interval
+  geom_hline(yintercept = lowerLOA_upperCI, linetype = 3 ) + # Lower limit of agreement - upper confidence interval
+  geom_hline(yintercept = lowerLOA_lowerCI, linetype = 3 ) + # Lower limit of agreement - lower confidence interval
+  annotate( "rect", xmin = -Inf , xmax = Inf , ymin = biasLowerCI , ymax = biasUpperCI , fill="blue" , alpha=0.3 ) + # Bias confidence interval shading
+  annotate( "rect", xmin = -Inf , xmax = Inf , ymin = upperLOA_lowerCI , ymax = upperLOA_upperCI , fill="green" , alpha=0.3 ) + # Upper limits of agreement confidence interval shading
+  annotate( "rect", xmin = -Inf , xmax = Inf , ymin = lowerLOA_lowerCI , ymax = lowerLOA_upperCI , fill="red" , alpha=0.3 ) # Lower limits of agreement confidence interval shading
+
+baplot
+
+
+
+}
+
+
+
+
+
+
+
+
