26528eba2dd7e8bcfd4943c58bb8296ffa52fd62
amanasj <67763567+amanasj@users.noreply.github.com>
2023-04-03 11:22:59 +0100

    Update BA_plot.R

--- R/BA_plot.R
+++ R/BA_plot.R
@@ -72,16 +72,22 @@ BA_plot <- function(stats,
   
   
   if("stimulus_ID" %in% colnames(data)){
-    baplot <- ggplot(data, aes(mean, diff)) +
-    if(show_patient_ID_cols = TRUE){
-        geom_point(aes(colour=patient_ID), size=pointsize) }else{
-        geom_point(colour=pointcolour, size=pointsize)} +
-      scale_color_viridis(discrete = T, option = "turbo") +
-      labs(x = x_label, 
-           y = y_label, 
-           title = title,
-           color = "Patient ID") +
-      theme
+
+         if(show_patient_ID_cols = TRUE){
+            baplot <- ggplot(data, aes(mean, diff)) +
+            geom_point(aes(colour=patient_ID), size=pointsize) 
+            scale_color_viridis(discrete = T, option = "turbo") +
+            labs(x = x_label, y = y_label, title = title, color = "Patient ID") +
+            theme
+    
+    }else{
+            baplot <- ggplot(data, aes(mean, diff)) +
+            geom_point(colour=pointcolour, size=pointsize)} +
+            scale_color_viridis(discrete = T, option = "turbo") +
+            labs(x = x_label, y = y_label, title = title, color = "Patient ID") +
+            theme
+    }
+    
     
   } else {
     baplot <- ggplot(data, aes(mean, diff)) +
