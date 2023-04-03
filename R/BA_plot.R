de2235f153de23e45eca32544e04370a7ea41a67
amanasj <67763567+amanasj@users.noreply.github.com>
2023-04-03 14:40:06 +0100

    Update BA_plot.R

--- R/BA_plot.R
+++ R/BA_plot.R
@@ -6,13 +6,13 @@ BA_plot <- function(stats,
                     title=NULL,
                     pointcolour="black", 
                     axis_xshift=0.1, 
-                    axis_yshift=0.1, 
-                    biaslabel_yshift=0.05, 
-                    biaslabel_xshift=-0.05,
-                    CIlabel_yshift=0.1, 
-                    CIlabel_xshift=-0.1,
-                    CoRlabel_yshift=0.9, 
-                    CoRlabel_xshift=0.8,
+                    axis_yshift=2, 
+                    biaslabel_yshift=0.08, 
+                    biaslabel_xshift=0.8,
+                    CIlabel_yshift=0.15, 
+                    CIlabel_xshift=0.8,
+                    CoRlabel_yshift=1.3, 
+                    CoRlabel_xshift=0.9,
                     digits=1,
                     pointsize=3.5,
                     axis_text_size=18,
@@ -55,21 +55,22 @@ BA_plot <- function(stats,
   
   ###################### extract stats for plots ###########################
   data <- stats$df
-  bias <- round(stats$summary$bias, digits = digits)
-  biasLowerCI <- round(stats$summary$bias_lowerCI, digits = digits)
-  biasUpperCI <- round(stats$summary$bias_upperCI, digits = digits)
-  lloa <- round(stats$summary$lower_LoA, digits = digits)
-  uloa <- round(stats$summary$upper_LoA, digits = digits)
-  upperLOA_upperCI <- round(stats$summary$upperLOA_upperCI, digits = digits)
-  upperLOA_lowerCI <- round(stats$summary$upperLOA_lowerCI, digits = digits)
-  lowerLOA_upperCI <- round(stats$summary$lowerLOA_upperCI, digits = digits)
-  lowerLOA_lowerCI <- round(stats$summary$lowerLOA_lowerCI, digits = digits)
-  CoR <- round(stats$summary$Coefficient_of_Repeatability, digits = digits)
+  bias <- stats$summary$bias
+  biasLowerCI <- stats$summary$bias_lowerCI
+  biasUpperCI <- stats$summary$bias_upperCI
+  lloa <- stats$summary$lower_LoA
+  uloa <- stats$summary$upper_LoA
+  upperLOA_upperCI <- stats$summary$upperLOA_upperCI
+  upperLOA_lowerCI <- stats$summary$upperLOA_lowerCI
+  lowerLOA_upperCI <- stats$summary$lowerLOA_upperCI
+  lowerLOA_lowerCI <- stats$summary$lowerLOA_lowerCI
+  CoR <- stats$summary$Coefficient_of_Repeatability
   
-
-  mean_max <- max(abs(data$mean))
-  diff_max <- max(abs(data$diff))
-
+  
+  mean_max <- abs(stats$df$mean)
+  mean_max <- max(mean_max)
+  diff_max <- max(c(upperLOA_upperCI, lowerLOA_lowerCI))
+  
   
   
   
@@ -110,17 +111,17 @@ BA_plot <- function(stats,
   baplot <- baplot + 
     ggplot2::coord_cartesian(xlim=c(xaxis_min, mean_max + (axis_xshift*mean_max)), ylim=c(-diff_max - (axis_yshift*diff_max), diff_max + (axis_yshift*diff_max)), expand = F)
   
-  baplot <- baplot+annotate("text", x=xaxis_min+(biaslabel_xshift*mean_max), y=biasUpperCI+(biaslabel_yshift*diff_max), 
-                            label= paste0("bias = ", bias, " (95% CI: ", biasLowerCI, "," , biasUpperCI,")"), 
+  baplot <- baplot+annotate("text", x=xaxis_min+(biaslabel_xshift*mean_max), y=bias+(biaslabel_yshift*diff_max), 
+                            label= paste0("bias = ", round(bias, digits = digits), " (95% CI: ", round(biasLowerCI, digits = digits), "," , round(biasUpperCI, digits = digits),")"), 
                             size=bias_type_size, fontface = 2)
-  baplot <- baplot+annotate("text", x=mean_max+(CIlabel_xshift*mean_max), y=upperLOA_upperCI+(CIlabel_yshift*diff_max), 
-                            label= paste0("ULoA = ", uloa, " (95% CI: ", upperLOA_lowerCI, "," , upperLOA_upperCI,")"), 
+  baplot <- baplot+annotate("text", x=xaxis_min+(CIlabel_xshift*mean_max), y=upperLOA_upperCI+(CIlabel_yshift*diff_max), 
+                            label= paste0("ULoA = ", round(uloa, digits = digits), " (95% CI: ", round(upperLOA_lowerCI, digits = digits), "," , round(upperLOA_upperCI, digits = digits),")"), 
                             size=LoA_type_size, fontface = 2)
-  baplot <- baplot+annotate("text", x=mean_max+(CIlabel_xshift*mean_max), y=lowerLOA_lowerCI-(CIlabel_yshift*diff_max), 
-                            label= paste0("LLoA = ", lloa, " (95% CI: ", lowerLOA_lowerCI, "," , lowerLOA_upperCI,")"), 
+  baplot <- baplot+annotate("text", x=xaxis_min+(CIlabel_xshift*mean_max), y=lowerLOA_lowerCI-(CIlabel_yshift*diff_max), 
+                            label= paste0("LLoA = ", round(lloa, digits = digits), " (95% CI: ", round(lowerLOA_lowerCI, digits = digits), "," , round(lowerLOA_upperCI, digits = digits),")"), 
                             size=LoA_type_size, fontface = 2) 
-  baplot <- baplot+annotate("text", x=mean_max+(CoRlabel_xshift*mean_max), y=biasUpperCI+(CoRlabel_yshift*diff_max), 
-                            label= paste0("CoR = ", CoR,"dB"), 
+  baplot <- baplot+annotate("text", x=xaxis_min+(CoRlabel_xshift*mean_max), y=diff_max+(CoRlabel_yshift*diff_max), 
+                            label= paste0("CoR = ", round(CoR, digits = digits),"dB"), 
                             size=CoR_type_size, fontface = 2) 
   baplot <- baplot +
     geom_hline(yintercept = bias , linetype = 2) + # Bias
@@ -132,17 +133,14 @@ BA_plot <- function(stats,
     geom_hline(yintercept = upperLOA_lowerCI, linetype = 3 ) + # Upper limit of agreement - lower confidence interval
     geom_hline(yintercept = lowerLOA_upperCI, linetype = 3 ) + # Lower limit of agreement - upper confidence interval
     geom_hline(yintercept = lowerLOA_lowerCI, linetype = 3 ) + # Lower limit of agreement - lower confidence interval
-    annotate( "rect", xmin = -Inf , xmax = Inf , ymin = biasLowerCI , ymax = biasUpperCI , fill="darkgrey" , alpha=alpha ) + # Bias confidence interval shading
-    annotate( "rect", xmin = -Inf , xmax = Inf , ymin = upperLOA_lowerCI , ymax = upperLOA_upperCI , fill="darkgrey" , alpha=alpha ) + # Upper limits of agreement confidence interval shading
-    annotate( "rect", xmin = -Inf , xmax = Inf , ymin = lowerLOA_lowerCI , ymax = lowerLOA_upperCI , fill="darkgrey" , alpha=alpha ) # Lower limits of agreement confidence interval shading
+    annotate( "rect", xmin = -Inf , xmax = Inf , ymin = biasLowerCI , ymax = biasUpperCI, fill="darkgrey", alpha=alpha) + # Bias confidence interval shading
+    annotate( "rect", xmin = -Inf , xmax = Inf , ymin = upperLOA_lowerCI, ymax = upperLOA_upperCI, fill="darkgrey" , alpha=alpha) + # Upper limits of agreement confidence interval shading
+    annotate( "rect", xmin = -Inf , xmax = Inf , ymin = lowerLOA_lowerCI, ymax = lowerLOA_upperCI, fill="darkgrey" , alpha=alpha) # Lower limits of agreement confidence interval shading
   
   baplot
   
   
   
 }
-
-
-
 
 
