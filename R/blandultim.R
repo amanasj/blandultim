cd8e075e754805a83ae32d05e6cb612d7783e092
Aman Josan <67763567+amanasj@users.noreply.github.com>
2024-03-26 15:45:20 +0000

    Update blandultim.R

--- R/blandultim.R
+++ R/blandultim.R
@@ -4,50 +4,43 @@
 #####################################################################################
 
 
-  
 
-blandultim <- function(data, x, y, 
-                       nboot=NULL, 
-                       fixed=NULL, 
-                       random=NULL, 
-                       bootstrap=NULL, 
-                       repeated=NULL){
 
-      
-  
-  if(bootstrap==TRUE){ 
-    
-      
-      cat("\n \n WARNING:- I have only written the bootstrapping code to deal with repeated measures so 'repeated=TRUE' 
+blandultim <- function(data, x, y, nboot=NULL, fixed=NULL, random=NULL, bootstrap=NULL, repeated=NULL){
+
+
+
+  if(bootstrap==TRUE){
+
+
+      cat("\n \n WARNING:- I have only written the bootstrapping code to deal with repeated measures so 'repeated=TRUE'
           is always ON by default and random and fixed variables MUST ALWAYS be specified when bootstrapping \n \n")
-    
-  
-    
-    tryCatch({
-      
-      
-  #### Run bootstrap script for non-parametric or repeated measures BA
-  stats <- parabootstraptLOA(nboot=nboot, data=data, x=x, y=y, seed=859, fixed=fixed, random=random)
-  
-  stats
-  
-    }, error=function(e){cat("\n \n ERROR : Must SPECIFY: 'NBOOT', 'RANDOM' AND 'FIXED'")})
-    
-    
 
-  
-  
-} else {
-  
-  
-  ### OR  run normal parametric non-repeated or repeated measures BA script
-  stats <- bland_altman_stats(df=data, x=x, y=y, random=random, fixed=fixed, repeated=repeated)
-  
+
+
+    tryCatch({
+
+
+  #### Run bootstrap script for non-parametric or repeated measures BA
+  stats <- blandultim::parabootstraptLOA(nboot=nboot, data=data, x=x, y=y, seed=859, fixed=fixed, random=random)
+
   stats
-  
+
+    }, error=function(e){cat("\n \n ERROR : Must SPECIFY: 'NBOOT', 'RANDOM' AND 'FIXED'")})
+
+
+
+
+
+} else {
+
+
+  ### OR  run normal parametric non-repeated or repeated measures BA script
+  stats <- blandultim::bland_altman_stats(df=data, x=x, y=y, random=random, fixed=fixed, repeated=repeated)
+
+  stats
+
   }
 
 }
-
-
 
